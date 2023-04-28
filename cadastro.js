ce4ae183f130568dc26b48ee7291ab27a5800d69
Maria Fernanda Castilho <castilhomariafer13@gmail.com>
2023-04-28 11:35:07 -0300

    finalizado arquivo de projeto

--- cadastro.js
+++ cadastro.js
@@ -5,15 +5,21 @@ function Validar(){
     let email = document.getElementById('email').value;
     let senha = document.getElementById('password').value;
     let name  = document.getElementById('name').value;
-    let dataNasc = document.getElementById('dataNasc').value;
+    let date  = document.getElementById('date').value;
     let fone = document.getElementById('fone').value;
-    let cpf = document.getElementById('CPF').value;
+    let cpf = document.getElementById('cpf').value;
     let rua = document.getElementById('rua').value;
+    let numero = document.getElementById('numero').value;
+    let cep = document.getElementById('cep').value;
+    let cidade = document.getElementById('cidade').value;
+    let bairro = document.getElementById('bairro').value;
+    let estado = document.getElementById('estado').value;
+    let complemento = document.getElementById('complemento').value;
     
+// || !cpf || !rua || !numero || !cep || cidade || !bairro || !estado || !complemento
 
-    if(!email || !senha || !name || !dataNasc || !fone || !cpf || !rua){
+    if(!email || !senha || !name || !date || !fone || !cpf || !rua || !numero || !cep || cidade || !bairro || !estado || !complemento){
         alert("Campos de preenchimento obrigatório. Favor preecher");
-        
     }else{
         alert("Campos preechidos com sucesso!");
     }
